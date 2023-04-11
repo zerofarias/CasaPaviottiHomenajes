ecc0ba24b2c027b0549f444fdb38677dd5916a63
lautaro <lautarosistemas@gmail.com>
2023-04-11 11:58:12 -0300

    correccion comentario y condolencias

--- FrontEnd/js/funcion.js
+++ FrontEnd/js/funcion.js
@@ -1,184 +1,198 @@
 $(document).ready(function () {
 
-/////////////////// RECIBE JSON de AJAX y LLAMA A FUNCION CAMBIO PANTALLA Y ESCRIBIR HTML////////////////////////
-    function cargarDato(data,canServicios) {
-      var inhumados = data;
-      let num = 0;
-      var cantidadServicios = canServicios;
-      console.log('cantidad de numeros q llega '+cantidadServicios);
-
-      setInterval(() => {
-        num = num + 1;
-        if (num < cantidadServicios) {
-          escribirHTML(inhumados, num);
-          cambiarPantalla(inhumados,cantidadServicios)
-          console.warn(num);
-        } else {
-          num = 0;
-          escribirHTML(inhumados, num);
-          cambiarPantalla(inhumados,cantidadServicios)
-        }
-      }, 26000);
-      ///3Min Son 180000
-
-      escribirHTML(inhumados, 0);
-      //cambiarPantalla(inhumados);
-    }
-
-  ////////////////// ESCRIBE DATOS EN HTML (screen0) //////////////////////
-    function escribirHTML(data, num) {
-      let dato = JSON.parse(data);
-
-      if (dato[num].foto != null) {
-        const fImagen = document.querySelector("[data-imgFallecido]");
-        $("#fimagen").attr("src", "../autogestion/images/" + dato[num].foto);
-      }
-      
-
-      const fNombre = document.querySelector("[data-nombreFallecido]");
-      fNombre.innerHTML = dato[num].apellido;
-
-      const fInfo = document.querySelector("[data-infoFallecido]");
-      fInfo.innerHTML = "El " + dato[num].fechafal + " A los " + dato[num].edad;
-
-      const fInhumacion = document.querySelector("[data-cementerio]");
-      fInhumacion.innerHTML =
-        "<b>INHUMACION </b>" + `<b>${dato[num].cementerio}</b>`;
-
-      const fInhuFechaHora = document.querySelector("[data-inhumacionFH]");
-      fInhuFechaHora.innerHTML =
-        `<b>${dato[num].fechasep}</b>` +
-        " a las " +
-        `<b>${dato[num].horasep}</b>`;
-
-      const contenedorInfoExtra = document.querySelector(
-        ".contenedor__infoExtra"
-      );
-      const codigoQR = document.querySelector("[data-qr]");
-      const fraseHomenaje = document.querySelector("#fraseHomenaje");
-      const body = document.querySelector("body");
-
-      let codigoExtinto = dato[num].COD_EXTINTO;
-        ajaxCondolencias(codigoExtinto);
-
-      generarQR(codigoExtinto + "99");
-      //cargarMsj(data,codigoExtinto)
-      //return codigoExtinto;
-
-
-    }
-
-  //////cambiar entre un pantalla y otra
-    function cambiarPantalla(inhumados,canServicios) {
-      let dato = JSON.parse(inhumados);
-      //let Cantidad = JSON.parse(canServicios);
-      //console.log('cantidad de numeros q llega for '+canServicios);
-            $("#screen0").hide();
-            $("#screen1").show();
-
-        for (let num = 0; num < canServicios; num++) {
-          
-            if(dato[num].apellido != undefined || dato[num].apellido != null){
-              $("#sala"+num).html(dato[num].apellido);
-            }
-
-            if (dato[num].foto != undefined) {
-              $("#img"+num).attr("src", "../autogestion/images/" + dato[num].foto);
-            }
-            if(dato[num].sala){
-              $(".salaVelatoria"+num).html(dato[num].sala);
-            }
-        }
-        setTimeout(()=> {
-            $("#screen1").hide();
-            $("#screen0").show();
-        },10000)
-    }
-    
-  ///////////// CONSULTA BASE DE DATOS DE PAVIOTTI/////////////////
-
-    function ajax() {
-      $.ajax({
-        url: "../../../CasaPaviottiHomenajes/back/logic/datos.php",
-        type: "POST",
-        datatype: "json",
-        data: { opcion: 3 },
-        success: function (cuenta) {
-          let conteo = JSON.parse(cuenta);
-          let cantServicio = conteo[0].CONTEO;
-              if (cantServicio > 0) {
-                $.ajax({
-                  url: "../../../CasaPaviottiHomenajes/back/logic/datos.php",
-                  type: "POST",
-                  datatype: "json",
-                  data: { opcion: 1 },
-                  success: function (data) {
-                    cargarDato(data,cantServicio);
-                    //console.warn("llama ajax");
-                  },
-                  error: function () {
-                      return null;
-                  },
-                });
-
-              }else{
-                return null;
-              }
-          
-        
-        },
-        error: function () {
-          console.error('fallo llamada Ajax');
-        },
-      });
-    }
-
-    /////////CONSULTO A BASE LAS CONDOLENCIAS DE EL INHUMADOS
-    function ajaxCondolencias(codigoExtinto){
-      $.ajax({
-        url: "../../../CasaPaviottiHomenajes/back/logic/datos.php",
-        type: "POST",
-        datatype: "json",
-        data: { opcion: 4, codigo:codigoExtinto },
-        success: function (cantCondolencias) {
-          let cantidad = JSON.parse(cantCondolencias);
-          let NumComentarios = cantidad[0].CANTCOMENTARIO;
-          if (NumComentarios > 0) {
-                $.ajax({
-                  url: "../../../CasaPaviottiHomenajes/back/logic/datos.php",
-                  type: "POST",
-                  datatype: "json",
-                  data: { opcion: 5, codigo:codigoExtinto },
-                  success: function (comentarios) {
-                    let msjCondolencias = JSON.parse(comentarios);
-                     // console.warn(msjCondolencias[1].mensaje);
-
-////////////////de aca ára abajo ////////////////////////////////////
-
-
-
-                      $("#comentario").html('<b>'+msjCondolencias[1].apellido+' '+msjCondolencias[1].nombre+'</b> <br>' +  msjCondolencias[1].mensaje);
-                  }
-                  
-                })
-
-          }else{
-            //// si NO HAY COMENTARIO 
-            console.log('NO HAY COMENTARIOS');
-          }
-            //////////// LA VARIABLE condolencias TIENE LOS COMENTARIOS DE CADA INHUMADOS
-        }
-      })
-    }
-
-  /////////////////Funcion que Genera El codigo QR (recibe el ID del Inhumado)
-    function generarQR(id) {
-    const contenedorQR = document.getElementById('contenedorQR');
-    contenedorQR.innerHTML= '';
-    const QR = new QRCode(contenedorQR,'https://paviotti.com.ar/CasaPaviottiHomenajes/envio-condolencias/index.php?condolencia='+id);
-    }
-
-    setInterval(ajax, 600000); ////Llamo Ajax Cada 10 Min
-    ajax(); ////LLamo a Ajax por Primera Vez
+  /////////////////// RECIBE JSON de AJAX y LLAMA A FUNCION CAMBIO PANTALLA Y ESCRIBIR HTML////////////////////////
+      function cargarDato(data,canServicios) {
+        var inhumados = data;
+        let num = 0;
+        var cantidadServicios = canServicios;
   
-}); ///FIN JQUERY
+        setInterval(() => {
+          num = num + 1;
+          if (num < cantidadServicios) {
+              escribirHTML(inhumados, num);
+              cambiarPantalla(inhumados,cantidadServicios)
+          } else {
+              num = 0;
+              escribirHTML(inhumados, num);
+              cambiarPantalla(inhumados,cantidadServicios)
+          }
+        }, 180000);
+        ///3Min Son 180000
+  
+        escribirHTML(inhumados, 0);
+      }
+  
+    ////////////////// ESCRIBE DATOS EN HTML (screen0) //////////////////////
+      function escribirHTML(data, num) {
+        let dato = JSON.parse(data);
+  
+        if (dato[num].foto != null) {
+          const fImagen = document.querySelector("[data-imgFallecido]");
+          $("#fimagen").attr("src", "../autogestion/images/" + dato[num].foto);
+        }
+        
+        const fNombre = document.querySelector("[data-nombreFallecido]");
+        fNombre.innerHTML = dato[num].apellido;
+
+        switch (dato[num].religion) {
+          case 1:
+            ////✝
+            break;
+          case 2:
+              //✡ 
+              break;
+          case 3:
+            //ॐ
+              break;
+          case 4:
+            //☪
+              break;
+          case 5:
+            //☸
+              break;
+          case 6:
+            //☯
+              break;
+          
+          default:
+            ////borra el circulo
+              break;
+        }
+
+
+
+
+  
+        const fInfo = document.querySelector("[data-infoFallecido]");
+        fInfo.innerHTML = "El " + dato[num].fechafal + " A los " + dato[num].edad;
+  
+        const fInhumacion = document.querySelector("[data-cementerio]");
+        fInhumacion.innerHTML =
+          "<b>INHUMACION </b>" + `<b>${dato[num].cementerio}</b>`;
+  
+        const fInhuFechaHora = document.querySelector("[data-inhumacionFH]");
+        fInhuFechaHora.innerHTML = `<b>${dato[num].fechasep}</b>` + " a las " + `<b>${dato[num].horasep}</b>`;
+  
+        const contenedorInfoExtra = document.querySelector(".contenedor__infoExtra");
+        const codigoQR = document.querySelector("[data-qr]");
+        const fraseHomenaje = document.querySelector("#fraseHomenaje");
+        const body = document.querySelector("body");
+
+        let codigoExtinto = dato[num].COD_EXTINTO;
+          ajaxCondolencias(codigoExtinto);
+  
+        generarQR(codigoExtinto + "99"); //// LLAMO A LA FUNCION QUE GENERA QR
+      }
+  
+    //////cambiar entre un pantalla y otra
+      function cambiarPantalla(inhumados,canServicios) {
+        let dato = JSON.parse(inhumados);
+              $("#screen0").hide();
+              $("#screen1").show();
+  
+          for (let num = 0; num < canServicios; num++) {
+            
+              if(dato[num].apellido != undefined || dato[num].apellido != null){
+                $("#sala"+num).html(dato[num].apellido);
+              }
+  
+              if (dato[num].foto != undefined) {
+                $("#img"+num).attr("src", "../autogestion/images/" + dato[num].foto);
+              }
+              if(dato[num].sala){
+                $(".salaVelatoria"+num).html(dato[num].sala);
+              }
+          }
+          setTimeout(()=> {
+              $("#screen1").hide();
+              $("#screen0").show();
+          },10000)
+      }
+    ///////////// CONSULTA BASE DE DATOS DE PAVIOTTI/////////////////
+  
+      function ajax() {
+        $.ajax({
+          url: "../../../CasaPaviottiHomenajes/back/logic/datos.php",
+          type: "POST",
+          datatype: "json",
+          data: { opcion: 3 },
+          success: function (cuenta) {
+            let conteo = JSON.parse(cuenta);
+            let cantServicio = conteo[0].CONTEO;
+                if (cantServicio > 0) {
+                  $.ajax({
+                    url: "../../../CasaPaviottiHomenajes/back/logic/datos.php",
+                    type: "POST",
+                    datatype: "json",
+                    data: { opcion: 1 },
+                    success: function (data) {
+                      cargarDato(data,cantServicio);
+                    },
+                    error: function () {
+                        return null;
+                    },
+                  });
+  
+                }else{
+                  return null;
+                }
+          },
+          error: function () {
+            console.error('fallo llamada Ajax');
+          },
+        });
+      }
+
+      /////////CONSULTO A BASE LAS CONDOLENCIAS DE EL INHUMADOS
+      function ajaxCondolencias(codigoExtinto){
+        $.ajax({
+          url: "../../../CasaPaviottiHomenajes/back/logic/datos.php",
+          type: "POST",
+          datatype: "json",
+          data: { opcion: 4, codigo:codigoExtinto },
+          success: function (cantCondolencias) {
+            let cantidad = JSON.parse(cantCondolencias);
+            let NumComentarios = cantidad[0].CANTCOMENTARIO;
+            if (NumComentarios > 0) {
+                  $.ajax({
+                      url: "../../../CasaPaviottiHomenajes/back/logic/datos.php",
+                      type: "POST",
+                      datatype: "json",
+                      data: { opcion: 5, codigo:codigoExtinto },
+                      success: function (comentarios) {
+                            mostrarCondolencia(comentarios,NumComentarios);
+                      }
+                  })
+            }else{
+              //// si NO HAY COMENTARIO 
+              let comentario = document.querySelector('[data-comentario]');
+              comentario.innerHTML = '';
+              comentario.innerHTML += `<p class="infoExtra"><strong>La muerte de un ser querido es siempre dolorosa, pero insluso esa tristeza da lugar a los momentos dedicados a recordar con una sonrisa a esa persona. Te deseo que muy pronto puedas hacerlo.</strong><br> Anonimo</p>`;
+            }
+          }
+        })
+      }
+
+      ////////////// ESCRIBE LAS CONDOLENCIAS EN LA PANTALLA ///////////////
+      function mostrarCondolencia(condolencia , NumComentarios){
+            let comentario = document.querySelector('[data-comentario]');
+            comentario.innerHTML = '';
+            let msjCondolencias = JSON.parse(condolencia); 
+            for (let i = 0; i < NumComentarios; i++){
+                    comentario.innerHTML += `<p class="infoExtra"><strong>${msjCondolencias[i].mensaje} </strong><br> ${msjCondolencias[i].nombre} ${msjCondolencias[i].apellido}</p>`;
+            }
+      }
+
+    /////////////////Funcion que Genera El codigo QR (recibe el ID del Inhumado)
+      function generarQR(id) {
+          const contenedorQR = document.getElementById('contenedorQR');
+          contenedorQR.innerHTML= '';
+          const QR = new QRCode(contenedorQR,'https://paviotti.com.ar/CasaPaviottiHomenajes/envio-condolencias/index.php?condolencia='+id);
+      }
+
+      
+  
+      setInterval(ajax, 600000); ////Llamo Ajax Cada 10 Min
+      ajax(); ////LLamo a Ajax por Primera Vez
+    
+  }); ///FIN JQUERY
--- FrontEnd/js/index.js
+++ FrontEnd/js/index.js
@@ -1,32 +1,31 @@
-const iconConfig = document.querySelector('.iconConf');
-const divEstilos = document.querySelector('.contenedor__estilos');
+const iconConfig = document.querySelector(".iconConf");
+const divEstilos = document.querySelector(".contenedor__estilos");
 // const estilos = document.querySelector('#estilos');
 // const estilo1 = document.querySelector('#estilo1');
 // const estilo2 = document.querySelector('#estilo2');
 // const estilo3 = document.querySelector('#estilo3');
 // const estilo4 = document.querySelector('#estilo4');
 // const estilo5 = document.querySelector('#estilo5');
-const fImagen = document.querySelector('[data-imgFallecido]');
-const fNombre = document.querySelector('[data-nombreFallecido]');
-const fInfo = document.querySelector('[data-infoFallecido]');
-const fInhumacion = document.querySelector('[data-inhumacion]');
-const fInhuFechaHora = document.querySelector('[data-inhumacionFH]');
-const contenedorInfoExtra = document.querySelector('.contenedor__infoExtra');
-const codigoQR = document.querySelector('[data-qr]');
-const infoExtra = document.querySelector('[data-mjsQR]');
-const fraseHomenaje = document.querySelector('#fraseHomenaje');
-const body = document.querySelector('body');
+const fImagen = document.querySelector("[data-imgFallecido]");
+const fNombre = document.querySelector("[data-nombreFallecido]");
+const fInfo = document.querySelector("[data-infoFallecido]");
+const fInhumacion = document.querySelector("[data-inhumacion]");
+const fInhuFechaHora = document.querySelector("[data-inhumacionFH]");
+const contenedorInfoExtra = document.querySelector(".contenedor__infoExtra");
+const codigoQR = document.querySelector("[data-qr]");
+const infoExtra = document.querySelector("[data-mjsQR]");
+const fraseHomenaje = document.querySelector("#fraseHomenaje");
+const body = document.querySelector("body");
 
 //PANTALLA SALAS VELATORIAS
-const imgDali = document.querySelector('[data-imgFallecidoDali]');
-const nombreDali = document.querySelector('[data-nombreFallecidoDali]');
+const imgDali = document.querySelector("[data-imgFallecidoDali]");
+const nombreDali = document.querySelector("[data-nombreFallecidoDali]");
 
-const imgPicasso = document.querySelector('[data-imgFallecidoPicasso]');
-const nombrePicasso = document.querySelector('[data-nombreFallecidoPicasso]');
+const imgPicasso = document.querySelector("[data-imgFallecidoPicasso]");
+const nombrePicasso = document.querySelector("[data-nombreFallecidoPicasso]");
 
-const imgVanGogh = document.querySelector('[data-imgFallecidoVanGogh]');
-const nombreVanGogh = document.querySelector('[data-nombreFallecidoVanGogh]');
-
+const imgVanGogh = document.querySelector("[data-imgFallecidoVanGogh]");
+const nombreVanGogh = document.querySelector("[data-nombreFallecidoVanGogh]");
 
 // body.addEventListener('mousemove', () => {
 //     iconConfig.classList.remove('inactive');
@@ -84,3 +83,26 @@ const nombreVanGogh = document.querySelector('[data-nombreFallecidoVanGogh]');
 //     infoExtra.style.color = 'var(--col-white)';
 //     fraseHomenaje.style.color = 'var(--col-white)';
 // })
+
+function escribirFrase(data, num) {
+  let dato = JSON.parse(data);
+
+  if (dato[num].mensaje != null) {
+    $("#contenedor__mensajes").html("La muerte de un ser querido es siempre dolorosa, pero insluso esa tristeza da lugar a los momentos dedicados a recordar con una sonrisa a esa persona. Te deseo que muy pronto puedas hacerlo.");
+    // return msjFrase;
+  }
+
+  for (let i = 0; i < msjCondolencias.length; i++) {
+    const frase = msjCondolencias[i];
+    console.log(frase.mensaje);
+    $("#contenedor__mensajes").html(`<p><strong>${frase[num].mensaje} </strong></p><br> ${frase[num].nombre} ${frase[num].apellido}`);
+  }
+
+  let codigoExtinto = dato[num].COD_EXTINTO;
+  ajaxCondolencias(codigoExtinto);
+
+}
+
+
+  
+$("#comentario").html('<b>' + frase.mensaje + '</b> <br>' + frase.apellido + ' ' + frase.nombre );
